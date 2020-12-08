f74fd2be617a0b3551b9215ac7a7c7472f946ec2
Vladimir Sokolov <vsokol74@mail.ru>
2020-12-09 01:26:09 +0300

    #1517 Каркас приложения

---
+++ src/test/java/milovanov/stc31/innopolis/checkuper/CheckuperApplicationTests.java
@@ -0,0 +1,13 @@
+package milovanov.stc31.innopolis.checkuper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckuperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
