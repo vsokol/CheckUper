2ba7db60339e049d108f78757edb54484a25a7b9
Vladimir Sokolov <vsokol74@mail.ru>
2020-12-10 23:49:21 +0300

    #1513 изменение названия таблиц

--- src/main/java/milovanov/stc31/innopolis/checkuper/sql/db-01-drop_object.sql
+++ src/main/java/milovanov/stc31/innopolis/checkuper/sql/db-01-drop_object.sql
@@ -1,8 +1,8 @@
 -- удаление всех объектов
-drop table if exists result_item;
-drop table if exists checklist_item;
-drop table if exists checklist_item_type;
-drop table if exists checklist;
+drop table if exists task_result;
 drop table if exists task;
+drop table if exists task_type;
+drop table if exists checklist;
+drop table if exists request;
 drop table if exists executor;
 drop table if exists customer;
