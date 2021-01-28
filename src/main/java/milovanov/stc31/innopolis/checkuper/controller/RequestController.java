9d9f840e9dee0e5f5434ebf8ab81d20373dfc637
Vladimir Sokolov <vsokol74@main.ru>
2021-01-28 21:55:54 +0300

    #1660 Просмотр списка моих заявок, заявок, которые можно взять в работу и заявок у меня в работе

--- src/main/java/milovanov/stc31/innopolis/checkuper/controller/RequestController.java
+++ src/main/java/milovanov/stc31/innopolis/checkuper/controller/RequestController.java
@@ -1,10 +1,13 @@
 package milovanov.stc31.innopolis.checkuper.controller;
 
+import milovanov.stc31.innopolis.checkuper.pojo.Customer;
 import milovanov.stc31.innopolis.checkuper.pojo.Executor;
 import milovanov.stc31.innopolis.checkuper.pojo.Request;
+import milovanov.stc31.innopolis.checkuper.pojo.User;
 import milovanov.stc31.innopolis.checkuper.service.IExecutorService;
 import milovanov.stc31.innopolis.checkuper.service.IRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,25 +37,33 @@ public class RequestController {
     @GetMapping
     public ModelAndView getAllRequests(@RequestParam(value = "executor_id", required = false) String paramExecutorId) {
         List<Request> requestList;
-        String title;
+        String title, viewType;
         try {
             Long executorId = Long.valueOf(paramExecutorId);
             Executor executor = executorService.getExecutorById(executorId);
             if (executor == null) {
                 requestList = new ArrayList<>();
                 title = "Исполнитель не найден";
+                viewType = "all";
             } else {
                 requestList = requestService.getAllRequestsByExecutor(executor);
                 title = executor.getName() + " (Заявки)";
+                viewType = "executor";
             }
         } catch (NumberFormatException exception) {
             requestList = requestService.getAllRequests();
             title = "Заявки";
+            viewType = "all";
         }
         ModelAndView modelAndView =
-                getModelAndView(requestList, title, "allrequests");
+                getModelAndView(requestList
+                        , title
+                        , viewType
+                        , "allrequests");
         return modelAndView;
     }
+
+
 
     /**
      * Просмотр указанного в <tt>request_id</tt> заказа.
@@ -109,6 +120,19 @@ public class RequestController {
         return modelAndView;
     }
 
+    /**
+     * Просмотр списка всех заказов, которые я создал
+     * @return ModelAndView с информацией по заказам и страницей отображения
+     */
+    @GetMapping(value = "/my")
+    public ModelAndView getRequestsCreatedMe(@AuthenticationPrincipal User user) {
+        Customer customer = user.getCustomer();
+        ModelAndView modelAndView =
+                getModelAndView(requestService.getAllRequestsByCustomer(customer)
+                        ,"Мои заявки", "my"
+                        , "user/workspase_applications");
+        return modelAndView;
+    }
 
     /**
      * Просмотр списка всех доступных для выполнения заказов
@@ -117,7 +141,23 @@ public class RequestController {
     @GetMapping(value = "/available")
     public ModelAndView getAllAvailableRequests() {
         ModelAndView modelAndView =
-                getModelAndView(requestService.getAllAvailableRequests(),"Доступные заявки", "allrequests");
+                getModelAndView(requestService.getAllAvailableRequests()
+                        ,"Доступные заявки", "available"
+                        , "user/workspase_applications_select");
+        return modelAndView;
+    }
+
+    /**
+     * Просмотр списка всех заказов, находящихся у меня в работе
+     * @return ModelAndView с информацией по заказам и страницей отображения
+     */
+    @GetMapping(value = "/progress")
+    public ModelAndView getMyRequestsInProgress(@AuthenticationPrincipal User user) {
+        Executor executor = user.getExecutor();
+        ModelAndView modelAndView =
+                getModelAndView(requestService.getAllRequestsByExecutor(executor)
+                        ,"Доступные заявки", "progress"
+                        , "user/workspase_applications_progress");
         return modelAndView;
     }
 
@@ -125,13 +165,15 @@ public class RequestController {
      * Возвращает ModelAndView с установленными необходимыми значениями
      * @param requestList список заявок
      * @param title название закладки
+     * @param viewType название элемента меню, который должен быть выбран
      * @param viewName название html
      * @return ModelAndView с установленными необходимыми значениями
      */
-    private ModelAndView getModelAndView(List<Request> requestList, String title, String viewName) {
+    private ModelAndView getModelAndView(List<Request> requestList, String title, String viewType,String viewName) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("title", title);
         modelAndView.addObject("requests", requestList);
+        modelAndView.addObject("viewType", viewType);
         modelAndView.setViewName(viewName);
         return modelAndView;
     }
