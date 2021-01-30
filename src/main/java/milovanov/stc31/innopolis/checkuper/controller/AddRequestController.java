6acc21daad3e31409795b9aaf4b2b8c8db18e4aa
Pavel Mamaev <mamai_88@mail.ru>
2021-01-30 15:47:23 +0300

    Task#1662 добавление задач к заявке более 1шт. Удалил лишние импорты.

--- src/main/java/milovanov/stc31/innopolis/checkuper/controller/AddRequestController.java
+++ src/main/java/milovanov/stc31/innopolis/checkuper/controller/AddRequestController.java
@@ -9,8 +9,6 @@ import milovanov.stc31.innopolis.checkuper.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
